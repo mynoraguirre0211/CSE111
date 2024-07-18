b8b08aec12cb92112409e3b258d99941468519f3
mynoraguirre0211 <mynor.aguirre.0211@gmail.com>
2024-07-18 07:36:04 -0600

    test

--- CSE111/test_words.py
+++ CSE111/test_words.py
@@ -28,6 +28,20 @@ def test_prefix():
     assert prefix("Disable", "dIstasteful") == "dis"
 
 
+def test_suffix():
+    suf = suffix("", "")
+    assert isinstance(suf, str)
+    
+    assert suffix("", "") == ""
+    assert suffix("", "correct") == ""
+    assert suffix("clear", "") == ""    
+    assert suffix("angelic", "awesome") == ""
+    assert suffix("found", "profound") == "found"
+    assert suffix("ditch", "itch") == "itch"
+    assert suffix("happy", "funny") == "y"
+    assert suffix("tired", "fatigued") == "ed"
+    assert suffix("swimming", "FLYING") == "ing"
+
 # Call the main function that is part of pytest so that the
 # computer will execute the test functions in this file.
 pytest.main(["-v", "--tb=line", "-rN", __file__])
